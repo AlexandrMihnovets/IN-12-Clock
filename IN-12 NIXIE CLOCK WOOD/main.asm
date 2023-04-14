48cfb5acc1a1fd3490d27dd30be4fbcd560ca97c
Alexandr Mikhnovets <sasha_mihnovets@mail.ru>
2023-04-14 15:20:56 +0500

    Make 0 1 2 3 4 5 6 7 8 9 correct

--- IN-12 NIXIE CLOCK WOOD/main.asm
+++ IN-12 NIXIE CLOCK WOOD/main.asm
@@ -17,8 +17,14 @@
 .def HOURS	 = r3				; Десятки и единицы часов в BCD-формате
 .def temp	 = r16				; Временная переменная
 .def Razryad = r17				; Для переключения индикаторов (значения от 0 до 5)
-.def Razr1	 = r18
-.def Razr0	 = r19
+;.def Razr1	 = r18
+;.def Razr0	 = r19
+.def eSec	= r18
+.def dSec	= r19
+.def eMin	= r20
+.def dMin	= r21
+.def eHour	= r22
+.def dHour	= r23
 .equ T1 = 6						; Индикатор десятков часов
 .equ T2 = 7						; Индикатор единиц часов
 .equ T3 = 5						; Индикатор десятков минут
@@ -39,6 +45,12 @@
 	.org SRAM_START		; Начало оперативной памяти (за всеми регистрами ввода-вывода)
 
 .CSEG ; Область флэш памяти ПЗУ ================================================================================================
+
+Numbers:
+; Массив цифр для дешифратора (не по порядку из-за дурацкой разводки)
+;	0	 1	  2	   3	4	 5	  6	   7	8	 9
+.db $00, $01, $04, $05, $08, $09, $0C, $0D, $02, $03
+
 ; Начало программы
 Reset:	
 	.include "ini.inc"	; Инициализация стека, перифирии и т.д.
@@ -46,19 +58,19 @@ Reset:
 	;sbi PORTC, 2
 	;sbi PORTC, 0
 
-	ldi ZL, low(Numbers*2)
-	ldi ZH, high(Numbers*2)
-
+	
 	;lpm temp, Z+
 	;out PORTC, temp
-
-	ldi temp, $14
+	/*
+	ldi temp, 0;$14
 	mov HOURS, temp
-	ldi temp, $58
+	ldi temp, 0;$58
 	mov MINUTES, temp
-	ldi temp, $9c
+	ldi temp, 0;$9c
 	mov SECONDS, temp
+	*/
 
+	clr temp
 
 	;inc r1
 	;inc r1
@@ -66,57 +78,53 @@ Reset:
     sei					; Разрешаю глобальные прерывания
 
 	LOOP:				; Основной цикл программы
+
+	ldi ZL, low(Numbers*2)
+	ldi ZH, high(Numbers*2)
 	
 		Switch_loop:
 				cpi Razryad, 1	; Если работает первый индикатор,
 				brne Ind_2
-				mov temp, HOURS	; достаю значение часов	
-				swap temp		; переворачиваю, т.к. мне нужны биты 3-0
-				andi temp, $0F	; ненужные биты обнуляю
+				add ZL, dHour
+				lpm temp, Z
 				out PORTC, temp	; устанавливаю необходимое значение на дешифратор
 
 			Ind_2:
 				cpi Razryad, 2
 				brne Ind_3
-				mov temp, HOURS
-				andi temp, $0F
+				add ZL, eHour
+				lpm temp, Z
 				out PORTC, temp
 
 			Ind_3:
 				cpi Razryad, 3
 				brne Ind_4
-				mov temp, MINUTES
-				swap temp
-				andi temp, $0F
+				add ZL, dMin
+				lpm temp, Z
 				out PORTC, temp
 
 			Ind_4:
 				cpi Razryad, 4
 				brne Ind_5
-				mov temp, MINUTES
-				andi temp, $0F
+				add ZL, eMin
+				lpm temp, Z
 				out PORTC, temp
 
 			Ind_5:
 				cpi Razryad, 5
 				brne Ind_6
-				mov temp, SECONDS
-				swap temp
-				andi temp, $0F
+				add ZL, dSec
+				lpm temp, Z
 				out PORTC, temp
 
 			Ind_6:
 				cpi Razryad, 6
 				brne End_switch_loop
-				mov temp, SECONDS
-				andi temp, $0F
+				add ZL, eSec
+				lpm temp, Z
 				out PORTC, temp
 
 		End_switch_loop:
 	
     rjmp LOOP
 
-Numbers:
-; Массив цифр для дешифратора (не по порядку из-за дурацкой разводки)
-;	0	 1	  2	   3	4	 5	  6	   7	8	 9
-.db $00, $01, $04, $05, $08, $09, $0C, $0D, $02, $03
